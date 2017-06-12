260ff2e0329c55a564b6f724e3fdea5520d1f1dd
zhoubo <zhoubo@xbniao.com>
2017-06-12 17:41:57 +0800

    添加了选择滤镜的view

---
+++ VideoDemo/ViewController/View/LZCameraFilterCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  LZCameraFilterCollectionView.h
+//  VideoDemo
+//
+//  Created by biyuhuaping on 2017/6/12.
+//  Copyright © 2017年 biyuhuaping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LZCameraFilterCollectionView : UICollectionView
+
+@end
---
+++ VideoDemo/ViewController/View/LZCameraFilterCollectionView.m
@@ -0,0 +1,21 @@
+//
+//  LZCameraFilterCollectionView.m
+//  VideoDemo
+//
+//  Created by biyuhuaping on 2017/6/12.
+//  Copyright © 2017年 biyuhuaping. All rights reserved.
+//
+
+#import "LZCameraFilterCollectionView.h"
+
+@implementation LZCameraFilterCollectionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
